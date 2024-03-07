bf85259044bac577d0cdbb65b5ca8d93c8e2380a
esposig2 <giuseppe.esposito2@mail.dcu.ie>
2024-03-07 17:57:18 +0000

    .

---
+++ year-02/ca269/forza4/diagonal.java
@@ -0,0 +1,55 @@
+class diagonal{
+	public void dig_trbl(int[][] matrix, int row, int col){
+		int n = row + col - 1;
+		int currRow, currCol;
+		boolean validRow, validCol;
+
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < row; j++){
+				currRow = 0 + j;
+				currCol = col - 1 - i + j;
+				validRow = 0 <= currRow && currRow < row;
+				validCol = 0 <= currCol && currCol < col;
+				if(validRow && validCol){
+					System.out.print("(" + matrix[currRow][currCol] + "),");
+				}
+			}
+			System.out.println("\n");
+		}
+	}
+
+	public void dig_tlbr(int[][] matrix, int row, int col){
+		int n = row + col - 1;
+		int currRow, currCol;
+		boolean validRow, validCol;
+
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < row; j++){
+				currRow = 0 + j;
+				currCol = 0 + i - j;
+				validRow = (0 <= currRow && currRow < row);
+				validCol = (0 <= currCol && currCol < col);
+				if(validRow && validCol){
+					System.out.print("("+matrix[currRow][currCol]+")");
+					if(currRow == 0){
+						System.out.print("<-first ");
+					}
+				}	
+			}
+			System.out.println("\n");
+		}
+	}
+
+
+
+	public static void main(String[] args) {
+		int[][] matrix = {
+			{1,2,3,10},
+			{4,5,6,11},
+			{7,8,9,12},
+		};
+		var a = new diagonal();
+		a.dig_trbl(matrix, 3, 4);
+		a.dig_tlbr(matrix, 3, 4);
+	}
+}
