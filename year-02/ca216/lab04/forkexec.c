208aaeb7e3f6d3d6e8bcb100dceb8c842a362d03
Giuseppe Esposito <giuseppe.esposito2@mail.dcu.ie>
2024-03-11 18:20:13 +0000

    .

--- year-02/ca216/lab04/forkexec.c
+++ year-02/ca216/lab04/forkexec.c
@@ -1,62 +1,28 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<sys/wait.h>
+#include<stdlib.h>
+#include<fcntl.h>
 
-void run_command(char *command, char **arg_list, int wait, int *pstatus);
+void run_command(char *command, char **arg_list, int wait, int output);
 
 int main(int argc, char **argv){
 
     pid_t pid;    
-    //char *command = "ls";
-    //char *argument_list[] = {"ls", "-l", NULL};
-    int status;
     int wait = 1;
     char *command = argv[1];
     char **argument_list = argv + 1;
 
+    //int file_desc = open("t.txt", O_CREAT | O_APPEND | O_WRONLY );// >>
+    int file_desc = open("t.txt", O_CREAT | O_TRUNC | O_WRONLY); // >
 
-    /*printf("%s\n", command);
-    while(*argument_list){
-        printf("%s\n", *argument_list++);
-    };*/
-
-    run_command(command, argument_list, wait, &status);
-    /*
-    printf("Before calling execvp()\n");
-
-    printf("Creating another process using fork()... \n");
-
-
-    pid = fork();
-    if( pid == -1 ){
-        //an error occured with the forking process
-        printf("Error with the forking process");
-        return 1;
-    }else if( pid == 0 ){
-        //newly spawned child process. This well be taken over by "ls -l"
-        int status_code = execvp(command, argument_list);
-
-        printf("ls -l has taken controlf of this child process. This won't execute unless it termiate wrong!\n");
-        
-        if( status_code == -1 ){
-            printf("Terminated Incorrectly\n");
-            return 1;
-        }
-    }else{
-        //old parent process. The c programm will come here
-
-        //we wait for the child process to finish if we need
-        if(wait)
-            waitpid(pid, &status, WUNTRACED);
-
-        printf("This line will be printed\n");
-    }
-*/
+    run_command(command, argument_list, wait, file_desc);
+    printf("ao");
 
     return 0;
 }
 
-void run_command(char *command, char **arg_list, int wait, int *pstatus){
+void run_command(char *command, char **arg_list, int wait, int output){
 
     printf("Before calling execvp we create another process with fork()\n");
     pid_t pid = fork();
@@ -65,6 +31,10 @@ void run_command(char *command, char **arg_list, int wait, int *pstatus){
         printf("Error while forking!\n");
     }else if(pid == 0){
         //child process that will be taken over by our command process called with execvp
+        if(output > 0){
+            dup2(output, STDOUT_FILENO); //ridirect the output to the file pointed by input
+            close(output);
+        }
         int status_code = execvp(command, arg_list);
         printf("Our command has taken control of this child process. This won't execute unless it termiate wrong!\n");
         if(status_code == -1){
@@ -73,7 +43,7 @@ void run_command(char *command, char **arg_list, int wait, int *pstatus){
     }else{
         //parent process
         if(wait)
-            waitpid(pid, pstatus, WUNTRACED);
+            waitpid(pid, NULL, WUNTRACED);
 
         printf("Hello from parent process\n");
     }
